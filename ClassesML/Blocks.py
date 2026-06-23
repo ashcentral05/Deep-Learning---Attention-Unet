40c976a447e7b6f4e56ff7bd082c02157d7fe190
ashcentral05 <ashvinsrimoorthy@gmail.com>
2026-06-23 12:19:26 +0200

    Update Blocks.py

--- ClassesML/Blocks.py
+++ ClassesML/Blocks.py
@@ -188,6 +188,22 @@ class UpConv2DBlock(nn.Module):
         x = self.activation(x)
         x = self.dropout_layer(x)
         return x
+
+class AttentionBlock(nn.Module):
+    def __init__(self, F_g, F_l, F_int,activation=nn.ReLU,dropout_rate=0.1):
+        super(AttentionBlock, self).__init__()
+        self.W_g = Conv2DBlock(in_channels=F_g,out_channels=F_int,kernel_size=1,stride=1,padding=0) 
+        self.W_x = nn.Sequential(nn.Conv2d(F_l, F_int, kernel_size=1, stride=1, padding=0, bias=True), nn.BatchNorm2d(F_int))
+        self.psi = nn.Sequential(nn.Conv2d(F_int, 1, kernel_size=1, stride=1, padding=0, bias=True), nn.BatchNorm2d(1), nn.Sigmoid())
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, g, x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        return x * psi
+    
 ######################################################################################
 class BasicResNetBlock(nn.Module):
     def __init__(
