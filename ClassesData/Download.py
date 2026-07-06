79263d76a211a11c8fabc9aa663573764ddbc307
Gabin Sagot <gabin.sagot@etu.minesparis.psl.eu>
2026-07-07 05:32:13 +0900

    Fixed Download.py

--- ClassesData/Download.py
+++ ClassesData/Download.py
@@ -36,15 +36,14 @@ def download_dataset(path, from_drive=False, size=128):
     else:
         kagglehub.dataset_download("bitsandlayers/sar-oil-spill-segmentation-dataset-sos", output_dir=path)
 
-path = os.path.join(os.getcwd(), "Dataset", "UNET")
-downloaded_path = os.path.join(path, "dataset")
-if downloaded_path and os.path.exists(downloaded_path):
-    for item in os.listdir(downloaded_path):
-        source_item = os.path.join(downloaded_path, item)
-        target_item = os.path.join(path, item)
-        
-        shutil.move(source_item, target_item)
-    
-    dossier_auteur = os.path.join(path, "dataset")
-    if os.path.exists(dossier_auteur):
-        shutil.rmtree(dossier_auteur)
+        downloaded_path = os.path.join(path, "dataset")
+        if downloaded_path and os.path.exists(downloaded_path):
+            for item in os.listdir(downloaded_path):
+                source_item = os.path.join(downloaded_path, item)
+                target_item = os.path.join(path, item)
+                
+                shutil.move(source_item, target_item)
+            
+            author_folder = os.path.join(path, "dataset")
+            if os.path.exists(author_folder):
+                shutil.rmtree(author_folder)
